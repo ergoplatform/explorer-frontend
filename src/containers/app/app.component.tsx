0cc9cb9617c70253b8ed5e060784f5c1b202211b
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-05-03 15:05:33 +0300

    Move components to pages

--- src/containers/app/app.component.tsx
+++ src/containers/app/app.component.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
 
-import DataComponent from '../../components/data/data.component';
-import NotFoundComponent from '../../components/not-found/not-found.component';
 import SidebarComponent from '../../components/sidebar/sidebar.component';
+import DataComponent from '../../pages/data/data.component';
+import NotFoundComponent from '../../pages/not-found/not-found.component';
 
 import './app.scss';
 
