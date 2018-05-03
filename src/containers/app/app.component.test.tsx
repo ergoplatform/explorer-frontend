6d212c0869e7713b252ff48cd7902b5a7e849754
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-05-03 15:32:56 +0300

    Fix travis integration

---
+++ src/containers/app/app.component.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// TODO: replace with real test
+it('App Renders without crashing', () => {
+  const div = document.createElement('div');
+  
+  ReactDOM.render(<div/>, div);
+});
