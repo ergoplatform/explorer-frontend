0106fc6083a3a17949de081fbd3b90454f837619
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-05-12 19:54:08 +0300

    Add search component test

---
+++ src/components/search/search.component.test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import { shallowWithIntl } from '../../utils/test-utils';
+
+import SearchComponent from './search.component';
+
+describe('Components | Search', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallowWithIntl(<SearchComponent/>);
+    
+    expect(wrapper.length)
+      .toBe(1);
+  });
+});
