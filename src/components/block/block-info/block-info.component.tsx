d227ede39693ca83d6c895e3c5335ccfb507371e
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-05-29 14:49:28 +0300

    Extend block info page

--- src/components/block/block-info/block-info.component.tsx
+++ src/components/block/block-info/block-info.component.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import { formatNumberMetricPrefix } from '../../../utils/formatNumberMetricPrefix';
 
 import { FullBlock } from '../../../models/generated/fullBlock';
 
@@ -13,7 +16,7 @@ export class BlockInfoComponent extends React.Component<IBlockInfoProps> {
   render (): JSX.Element {
     return (
       <div className='bi-block-info'>
-        <div className='bi-block-info__table'>
+        <div className='bi-block-info__table bi-table'>
           <div className='bi-block-info__row'>
             <div className='bi-block-info__cell bi-block-info__cell--header'>
               <FormattedMessage id='common.block.height'/>
@@ -44,13 +47,116 @@ export class BlockInfoComponent extends React.Component<IBlockInfoProps> {
             </div>
           </div>
           
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.parent'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              <Link to={ `/blocks/${this.props.block.header.parentId}` }>
+                { this.props.block.header.parentId }
+              </Link>
+            </div>
+          </div>
+          
           <div className='bi-block-info__row'>
             <div className='bi-block-info__cell bi-block-info__cell--header'>
               <FormattedMessage id='common.block.nonce'/>
             </div>
             
             <div className='bi-block-info__cell'>
-              { this.props.block.header.nonce }
+              { this.props.block.header.extensionHash }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.version'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.version }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.interlinks'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.interlinks.join(' ') }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.adProofsRoot'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.adProofsRoot }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.transactionsRoot'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.transactionsRoot }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.stateRoot'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.stateRoot }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.nBits'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.nBits }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.votes'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { this.props.block.header.votes }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.equihashSolutions'/>
+            </div>
+            
+            <div className='bi-block-info__cell u-word-wrap'>
+              { this.props.block.header.equihashSolutions }
+            </div>
+          </div>
+          
+          <div className='bi-block-info__row'>
+            <div className='bi-block-info__cell bi-block-info__cell--header'>
+              <FormattedMessage id='common.block.size'/>
+            </div>
+            
+            <div className='bi-block-info__cell'>
+              { formatNumberMetricPrefix(this.props.block.header.blockSize, 'k') }B
             </div>
           </div>
         </div>
