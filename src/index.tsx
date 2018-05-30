2accfc819ae69c9c71754d0abe9f5bc75c1edd6b
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-05-30 14:15:11 +0300

    Remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,6 @@ import * as ru from 'react-intl/locale-data/ru';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import registerServiceWorker from './utils/registerServiceWorker';
-
 import './assets/styles/main.scss';
 
 import './config/axios.config';
@@ -45,4 +43,3 @@ ReactDOM.render(
 );
 
 enableTabMode();
-registerServiceWorker();
