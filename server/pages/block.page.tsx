85ea38e17baacabf0f8134f40f901ced36c78c29
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-06-11 16:45:19 +0300

    Block page ssr

---
+++ server/pages/block.page.tsx
@@ -0,0 +1,33 @@
+import * as express from 'express';
+
+import { GET_BLOCK_SUCCESS } from '../../client/src/constants/block.types';
+import { blockReducer, initialState } from '../../client/src/reducers/block.reducer';
+import { BlockApiService } from '../../client/src/services/block.api.service';
+
+export const BlockPage = express.Router();
+
+const render = (req: any, res: any, next: any) => {
+  console.debug(req.params.id);
+  
+  BlockApiService.getBlock(req.params.id)
+    .then((data: any) => {
+      const preloadedState = blockReducer(initialState, {
+        payload: {
+          data,
+        },
+        type: GET_BLOCK_SUCCESS,
+      });
+      
+      req.explorer.preloadedState = {
+        ...req.explorer.preloadedState,
+        block: preloadedState,
+      };
+      
+      next();
+    });
+};
+
+
+BlockPage.get('/:id', render);
+BlockPage.get('/:id/transactions', render);
+BlockPage.get('/:id/adproofs', render);
