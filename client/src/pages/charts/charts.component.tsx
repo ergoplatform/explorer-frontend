91dbafab5277319726ffd002c71365e20542431d
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-06-28 15:58:35 +0300

    Fix charts UI

--- client/src/pages/charts/charts.component.tsx
+++ client/src/pages/charts/charts.component.tsx
@@ -34,7 +34,7 @@ class Charts extends React.PureComponent<StatsActions & StatsState> {
         
         <div className='bi-charts__header g-flex-column__item-fixed'>
           <div className='bi-charts__title'>
-            Charts
+            <FormattedMessage id='components.charts.title'/>
           </div>
         </div>
         
@@ -55,13 +55,13 @@ class Charts extends React.PureComponent<StatsActions & StatsState> {
         </div>
         <div className='bi-charts__charts-wrapper g-flex-column__item'>
           <div className='bi-charts__title'>
-            Currency Statistics
+            <FormattedMessage id='components.charts.currency-statistics'/>
           </div>
     
           <div className='bi-charts__charts'>
             <Link to={ '/charts/total' } className='bi-charts__chart'>
-              Total coins per date
-        
+              <FormattedMessage id='components.chart.title.total'/>
+  
               <iframe src='/charts/total?iframe=true'
                       className='bi-charts__iframe'
                       frameBorder='0'
@@ -72,29 +72,35 @@ class Charts extends React.PureComponent<StatsActions & StatsState> {
         </div>
         <div className='bi-charts__charts-wrapper g-flex-column__item'>
           <div className='bi-charts__title'>
-            Block Details
+            <FormattedMessage id='components.charts.block-details'/>
           </div>
     
           <div className='bi-charts__charts'>
             <Link to={ '/charts/block-size' } className='bi-charts__chart'>
-              Average block size
-        
-              <iframe src='/charts/block-size?iframe=true' className='bi-charts__iframe' frameBorder='0' scrolling='no'
+              <FormattedMessage id='components.chart.title.block-size'/>
+  
+              <iframe src='/charts/block-size?iframe=true'
+                      className='bi-charts__iframe'
+                      frameBorder='0'
+                      scrolling='no'
                       tabIndex={ -1 }/>
             </Link>
       
             <Link to={ '/charts/blockchain-size' } className='bi-charts__chart'>
-              Blockchain size
-        
+              <FormattedMessage id='components.chart.title.blockchain-size'/>
+  
               <iframe src='/charts/blockchain-size?iframe=true' className='bi-charts__iframe' frameBorder='0'
                       scrolling='no' tabIndex={ -1 }/>
             </Link>
       
             <Link to={ '/charts/transactions-per-block' } className='bi-charts__chart'>
-              Transactions per Block
-        
-              <iframe src='/charts/transactions-per-block?iframe=true' className='bi-charts__iframe' frameBorder='0'
-                      scrolling='no' tabIndex={ -1 }/>
+              <FormattedMessage id='components.chart.title.transactions-per-block'/>
+  
+              <iframe src='/charts/transactions-per-block?iframe=true'
+                      className='bi-charts__iframe'
+                      frameBorder='0'
+                      scrolling='no'
+                      tabIndex={ -1 }/>
             </Link>
     
           </div>
@@ -102,29 +108,38 @@ class Charts extends React.PureComponent<StatsActions & StatsState> {
         
         <div className='bi-charts__charts-wrapper g-flex-column__item'>
           <div className='bi-charts__title'>
-            Mining Infromation
+            <FormattedMessage id='components.charts.mining-information'/>
           </div>
           
           <div className='bi-charts__charts'>
             <Link to={ '/charts/hash-rate' } className='bi-charts__chart'>
-              Hash Rate
-    
-              <iframe src='/charts/hash-rate?iframe=true' className='bi-charts__iframe' frameBorder='0'
-                      scrolling='no' tabIndex={ -1 }/>
+              <FormattedMessage id='components.chart.title.hash-rate'/>
+  
+  
+              <iframe src='/charts/hash-rate?iframe=true'
+                      className='bi-charts__iframe'
+                      frameBorder='0'
+                      scrolling='no'
+                      tabIndex={ -1 }/>
             </Link>
   
   
             <Link to={ '/charts/difficulty' } className='bi-charts__chart'>
-              Difficulty
-    
-              <iframe src='/charts/difficulty?iframe=true' className='bi-charts__iframe' frameBorder='0'
-                      scrolling='no' tabIndex={ -1 }/>
+              <FormattedMessage id='components.chart.title.difficulty'/>
+  
+              <iframe src='/charts/difficulty?iframe=true'
+                      className='bi-charts__iframe'
+                      frameBorder='0'
+                      scrolling='no'
+                      tabIndex={ -1 }/>
             </Link>
             
             <Link to={ '/charts/miners-revenue' } className='bi-charts__chart'>
-              Miners Revenue
-    
-              <iframe src='/charts/miners-revenue?iframe=true' className='bi-charts__iframe' frameBorder='0'
+              <FormattedMessage id='components.chart.title.miners-revenue'/>
+  
+              <iframe src='/charts/miners-revenue?iframe=true'
+                      className='bi-charts__iframe'
+                      frameBorder='0'
                       scrolling='no' tabIndex={ -1 }/>
             </Link>
           </div>
