1bde22280eb3fa2919fca9cd246cf1293890eb73
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-06-30 17:41:59 +0300

    Cover timestamp component with tests

---
+++ client/src/components/common/timestamp/timestamp.component.test.tsx
@@ -0,0 +1,40 @@
+import { ShallowWrapper } from 'enzyme';
+import * as React from 'react';
+import dayjs from 'dayjs';
+
+import { shallowWithIntl } from '../../../utils/test-utils';
+
+import { TimestampComponent } from './timestamp.component';
+
+describe('Components | Timestamp', () => {
+  let date: Date;
+  let wrapper: ShallowWrapper;
+  
+  beforeEach(() => {
+    date    = new Date();
+    wrapper = shallowWithIntl(<TimestampComponent timestamp={ date.getTime() }/>);
+  });
+  
+  it('should render without crashing', () => {
+    expect(wrapper.length)
+      .toBe(1);
+  });
+  
+  it('should render correct format', () => {
+    expect(
+      wrapper.find('span')
+        .first()
+        .text()
+    )
+      .toEqual(dayjs(date)
+        .format('HH:mm:ss'));
+  
+    expect(
+      wrapper.find('span')
+        .last()
+        .text()
+    )
+      .toEqual(dayjs(date)
+        .format('DD.MM.YYYY'));
+  });
+});
