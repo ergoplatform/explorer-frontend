7e728b3047b600f54b40b6eaaaa02272ebe12064
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-06-30 14:54:48 +0300

    Cover Address summary with tests

---
+++ client/src/components/address/address-summary/address-summary.component.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { shallowWithIntl } from '../../../utils/test-utils';
+import { AddressSummaryComponent } from './address-summary.component';
+
+import { FullAddressSummary } from '../../../models/generated/fullAddressSummary';
+
+describe('Component | Address Summary', () => {
+  let summary: FullAddressSummary;
+  
+  beforeEach(() => {
+    summary = {
+      id: 'foo'
+    };
+  });
+  
+  it('renders without crashing', () => {
+    const wrapper = shallowWithIntl(<AddressSummaryComponent summary={ summary }/>);
+    
+    expect(wrapper.length)
+      .toBe(1);
+  });
+});
