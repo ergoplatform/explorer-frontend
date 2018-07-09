7104f844c914fea05516d0be0e24db3d0160638f
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-07-09 22:36:59 +0300

    Simple caching

--- server/pages/chart-image.tsx
+++ server/pages/chart-image.tsx
@@ -1,11 +1,31 @@
 import * as express from 'express';
+import fs from 'fs';
 import puppeteer from 'puppeteer';
-import tmp from 'tmp';
+import * as util from 'util';
 import environment from '../../client/src/config/environment';
 
 export const ChartImage = express.Router();
 
 ChartImage.get('/:chartType', async (req, res) => {
+  // TODO: replace with nginx caching
+  const cacheRoot = '/var/cache/explorer/';
+  const filename = cacheRoot + req.params.chartType;
+  
+  if (!fs.existsSync(cacheRoot)) {
+    fs.mkdirSync(cacheRoot);
+  }
+  
+  
+  if (fs.existsSync(filename)) {
+    const stats = fs.statSync(filename);
+    const mtime = new Date(util.inspect(stats.mtime)).getTime();
+    
+    if ((new Date()).getTime() - mtime < 60 * 60 * 60 ) {
+      return res.sendFile(filename);
+    }
+  }
+  
+  
   if (environment.environments) {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--headless', '--disable-gpu'],
@@ -21,16 +41,14 @@ ChartImage.get('/:chartType', async (req, res) => {
     
     await page.goto(environment.environments[0].url + '/charts/' + req.params.chartType + '?iframe=1');
     
-    const tmpPath = tmp.fileSync();
     
-    await page.waitFor(1500);
-    await page.screenshot({ path: tmpPath.name, type: 'jpeg' });
+    await page.waitFor(2000);
+    await page.screenshot({ path:filename, type: 'jpeg' });
     
     await browser.close();
     
-    
     res.setHeader('Expires', new Date(Date.now() + 60 * 60 * 60).toUTCString());
-    res.sendFile(tmpPath.name, { headers: { 'Content-Type': 'image/jpeg' } });
+    res.sendFile(filename, { headers: { 'Content-Type': 'image/jpeg' } });
   } else {
     res.sendFile('../../empty-photo.jpg', { headers: { 'Content-Type': 'image/jpeg' } });
   }
