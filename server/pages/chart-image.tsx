932d8ef58779f290e68eacddfee8666aacac157a
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-07-10 12:25:32 +0300

    Fix caching time

--- server/pages/chart-image.tsx
+++ server/pages/chart-image.tsx
@@ -7,7 +7,6 @@ import environment from '../../client/src/config/environment';
 export const ChartImage = express.Router();
 
 ChartImage.get('/:chartType', async (req, res) => {
-  // TODO: replace with nginx caching
   const cacheRoot = '/var/cache/explorer/';
   const filename = cacheRoot + req.params.chartType;
   
@@ -15,16 +14,15 @@ ChartImage.get('/:chartType', async (req, res) => {
     fs.mkdirSync(cacheRoot);
   }
   
-  
   if (fs.existsSync(filename)) {
     const stats = fs.statSync(filename);
     const mtime = new Date(util.inspect(stats.mtime)).getTime();
     
-    if ((new Date()).getTime() - mtime < 60 * 60 * 1000 ) {
+    // Random caching for images, increase cache time randomly from 1 to 100 sec
+    if ((new Date()).getTime() - mtime < 60 * 60 * 1000 + Math.random() * 100 * 1000 ) {
       return res.sendFile(filename);
     }
   }
-  
   
   if (environment.environments) {
     const browser = await puppeteer.launch({
@@ -41,13 +39,11 @@ ChartImage.get('/:chartType', async (req, res) => {
     
     await page.goto(environment.environments[0].url + '/charts/' + req.params.chartType + '?iframe=1');
     
-    
     await page.waitFor(2000);
     await page.screenshot({ path:filename, type: 'jpeg' });
     
     await browser.close();
     
-    res.setHeader('Expires', new Date(Date.now() + 60 * 60 * 1000).toUTCString());
     res.sendFile(filename, { headers: { 'Content-Type': 'image/jpeg' } });
   } else {
     res.sendFile('../../empty-photo.jpg', { headers: { 'Content-Type': 'image/jpeg' } });
