5ec330e056e4cd86463e5c24132459d76c870e1e
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-08-21 15:31:18 +0300

    Fix timespan for alltime

--- client/src/services/chart.api.service.ts
+++ client/src/services/chart.api.service.ts
@@ -3,11 +3,12 @@ import axios, { AxiosResponse } from 'axios';
 import environment from '../config/environment';
 
 import { IChartParams } from '../actions/chart.actions';
+
 import { TIMESPAN } from '../constants/timespan.constant';
 
 export class ChartApiService {
   static getChart (chartType: string, options: IChartParams = {}): any {
-    const timespan = options.timespan === 'alltime' ? '' : options.timespan;
+    const timespan = options.timespan === TIMESPAN.ALLTIME ? 'all' : options.timespan;
     
     return axios.get(`${environment.apiUrl}/charts/${chartType}`, {
       params: {
