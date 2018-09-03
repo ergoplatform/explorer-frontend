192881526d9471fc4546c04ee2faed98fccbd67c
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-09-03 13:46:17 +0300

    Add request timeout

--- client/src/config/axios.config.ts
+++ client/src/config/axios.config.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
 axios.defaults.headers.common = {
-  'Accept': 'application/json',
+  'Accept': 'application/json'
 };
+
+const timeout = process.env.ERGO_EXPLORER_TIMEOUT;
+
+axios.defaults.timeout = timeout ? parseInt(timeout, 10) : 60000;
+
+axios.interceptors.response.use((request: any) => {
+  return request;
+},  (error) => {
+  if (error.code === 'ECONNABORTED') {
+    console.error(`[ERROR] Request Timeout ${error.config.url}`);
+    
+    return false;
+  }
+  
+  return Promise.reject(error);
+});
