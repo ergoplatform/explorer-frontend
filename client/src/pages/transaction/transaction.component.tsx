c181c3101a687294f3dd55d7a866b3e456527545
Deadit <stdeadit@gmail.com>
2019-05-24 12:23:44 +0300

    init

--- client/src/pages/transaction/transaction.component.tsx
+++ client/src/pages/transaction/transaction.component.tsx
@@ -24,23 +24,23 @@ class Transaction extends React.PureComponent {
   props: RouteComponentProps<{
     id: string;
   }> & TransactionState & TransactionActions & SettingsActions & SettingsState;
-  
+
   constructor (props: any) {
     super(props);
-    
+
     this.onScriptToggle = this.onScriptToggle.bind(this);
   }
-  
+
   componentDidMount (): void {
     this.props.getTransaction(this.props.match.params.id);
   }
-  
+
   componentWillReceiveProps (nextProps: any): void {
     if (nextProps.match.params.id !== this.props.match.params.id) {
       this.props.getTransaction(nextProps.match.params.id);
     }
   }
-  
+
   render (): JSX.Element {
     return (
       <div className='bi-transaction'>
@@ -56,19 +56,19 @@ class Transaction extends React.PureComponent {
       </div>
     );
   }
-  
+
   private renderBody (): JSX.Element | null {
     if (!this.props.transaction) {
       return null;
     }
-    
+
     const transaction: TransactionModel = {
       id: this.props.transaction.summary.id,
       inputs: this.props.transaction.inputs,
       outputs: this.props.transaction.outputs,
       timestamp: this.props.transaction.summary.timestamp
     };
-    
+
     return (
       <div className='bi-transaction__body'>
         <FormattedMessage id='common.pages.transaction.title' values={ { id: this.props.transaction.summary.id } }>
@@ -80,45 +80,44 @@ class Transaction extends React.PureComponent {
             )
           }
         </FormattedMessage>
-        
         <TransactionsItemComponent transaction={ transaction }
                                    confirmations={ this.props.transaction.summary.confirmationsCount }/>
-        
+
         <div className='bi-transaction__tables g-flex'>
           <div className='bi-transaction__table g-flex__item'>
             <TransactionSummaryComponent summary={ this.props.transaction.summary }/>
           </div>
-          
+
           <div className='bi-transaction__table g-flex__item'>
             <TransactionIoSummaryComponent summary={ this.props.transaction.ioSummary }
                                            isScriptShown={ this.props.isScriptsDisplayed }
                                            onScriptToggle={ this.onScriptToggle }/>
           </div>
         </div>
-        
+
         { this.props.isScriptsDisplayed &&
         <div className='bi-transaction__scripts'>
           <div className='bi-transaction__title'>
             <FormattedMessage id='components.transaction.scripts.input'/>
           </div>
-          
+
           <TransactionRawScriptsComponent items={ this.props.transaction.inputs }/>
         </div>
         }
-        
+
         { this.props.isScriptsDisplayed &&
         <div className='bi-transaction__scripts'>
           <div className='bi-transaction__title'>
             <FormattedMessage id='components.transaction.scripts.output'/>
           </div>
-          
+
           <TransactionRawScriptsComponent items={ this.props.transaction.outputs }/>
         </div>
         }
       </div>
     );
   }
-  
+
   private onScriptToggle (): void {
     this.props.setTransactionScripts(!this.props.isScriptsDisplayed);
   }
