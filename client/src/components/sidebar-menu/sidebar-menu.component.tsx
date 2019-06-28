c3d9efdd9a119e24f0c80eb5b4fc6c5c1f3fefbb
Dmitry Usov <stdeadit@gmail.com>
2019-06-28 15:23:44 +0300

    update wallet link in sidebar (#37)
    * update wallet link in sidebar

--- client/src/components/sidebar-menu/sidebar-menu.component.tsx
+++ client/src/components/sidebar-menu/sidebar-menu.component.tsx
@@ -9,6 +9,7 @@ export interface ISidebarMenuItem {
   title: string;
   url: string;
   component?: any;
+  external?: boolean;
 }
 
 export interface ISidebarMenuProps {
@@ -24,11 +25,37 @@ export class SidebarMenuComponent extends React.Component<ISidebarMenuProps> {
       { this.mapLinks(this.props.items) }
     </div>;
   }
-  
+
   private mapLinks (items: ISidebarMenuItem[]): any {
     return items.map((item, index) => {
+      const isExternalLink = item.external || false;
       const Component = item.component || NavLink;
-      
+
+      if (isExternalLink) {
+        return (
+          <div className='bi-sidebar-menu__item g-flex-column__item' key={ index }>
+            <a className='bi-sidebar-menu__item-wrapper g-flex'
+                       activeClassName='bi-sidebar-menu__item-wrapper--active'
+                       href={ item.url }
+                       onClick={ this.props.onClick }
+                       { ...item.props }
+                       target="_blank">
+              { item.icon }
+
+              <span className='bi-sidebar-menu__item-title g-flex__item'>
+                <FormattedMessage id={ item.title }/>
+              </span>
+            </a>
+
+            { item.children && (
+              <div className='bi-sidebar-menu__item-children g-flex-column'>
+                { this.mapLinks(item.children) }
+              </div>
+            ) }
+          </div>
+        );
+      }
+
       return (
         <div className='bi-sidebar-menu__item g-flex-column__item' key={ index }>
           <Component className='bi-sidebar-menu__item-wrapper g-flex'
@@ -38,12 +65,12 @@ export class SidebarMenuComponent extends React.Component<ISidebarMenuProps> {
                      onClick={ this.props.onClick }
                      { ...item.props }>
             { item.icon }
-            
+
             <span className='bi-sidebar-menu__item-title g-flex__item'>
               <FormattedMessage id={ item.title }/>
             </span>
           </Component>
-          
+
           { item.children && (
             <div className='bi-sidebar-menu__item-children g-flex-column'>
               { this.mapLinks(item.children) }
