436b21346d07710c4780f6ac1c3019d99dc9954f
deadit <d-usov@city-mobil.ru>
2020-05-03 12:05:37 +0300

    add build

---
+++ build/precache-manifest.5916638d033f3885129ea6d9b3448694.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520e745afc2414b2d5e136d8113cc2e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c350cfabfbcb306d4e30",
+    "url": "/static/css/2.61c1edad.chunk.css"
+  },
+  {
+    "revision": "1e888720466433c94cd0",
+    "url": "/static/css/main.75206389.chunk.css"
+  },
+  {
+    "revision": "c350cfabfbcb306d4e30",
+    "url": "/static/js/2.c350cfab.chunk.js"
+  },
+  {
+    "revision": "ab5191c542967136e5e0f8832d001400",
+    "url": "/static/js/2.c350cfab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e888720466433c94cd0",
+    "url": "/static/js/main.1e888720.chunk.js"
+  },
+  {
+    "revision": "48bb0711bc8582eac657",
+    "url": "/static/js/runtime-main.48bb0711.js"
+  },
+  {
+    "revision": "fd572c9f58e6ff7b79abfebffce409a1",
+    "url": "/static/media/blockchain-size.fd572c9f.jpeg"
+  },
+  {
+    "revision": "0bfd3cf0f96f5c60f6c518cfe9a5303a",
+    "url": "/static/media/hash-rate-distribution.0bfd3cf0.jpeg"
+  },
+  {
+    "revision": "70699649c5af6c6b454d0d2c538b2b2b",
+    "url": "/static/media/hash-rate.70699649.jpeg"
+  },
+  {
+    "revision": "b6e848d8cc05db2678c24c8997cb48ca",
+    "url": "/static/media/miners-revenue.b6e848d8.jpeg"
+  }
+]);
