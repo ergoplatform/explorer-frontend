0e51d097b98bb05717b9a19e306e858ff01f406f
YBLex <alex56599@yandex.ru>
2020-12-22 20:08:27 +0300

    Update copy-text component

--- src/components/common/copy-text/copy-text.component.tsx
+++ src/components/common/copy-text/copy-text.component.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
+import { CopyIcon } from '../icons/common.icons';
 
 interface ICopyTextProps {
-  children: string | number;
+  children: string;
 }
 
 export class CopyTextComponent extends React.Component<ICopyTextProps> {
-  copyToClipboard(event: any) {
-    const textarea = document.createElement('textarea');
-    textarea.value = event.target.textContent || '';
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
+  constructor(props: ICopyTextProps) {
+    super(props);
+
+    this.copyToClipboard = this.copyToClipboard.bind(this);
   }
 
   render() {
-    // TODO add icon and tooltip "copied"
     return (
-      <a
-        className="bi-copy-text"
-        style={{ cursor: 'pointer' }}
+      <span
         onClick={this.copyToClipboard}
+        style={{ cursor: 'pointer', color: 'rgb(0, 120, 255);' }}
       >
-        {this.props.children}
-      </a>
+        <a className="bi-copy-text" style={{ marginRight: '10px' }}>
+          {this.props.children}
+        </a>
+        <CopyIcon />
+      </span>
     );
+  }
+
+  private copyToClipboard() {
+    const textContent = this.props.children;
+    const textarea = document.createElement('textarea');
+
+    textarea.value = textContent || '';
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
   }
 }
