a4d15b292b254fb946f78fabb761dc0e75748d0c
Luivatra <luivatra@gmail.com>
2021-10-22 19:58:08 +0200

    Switched eth-usd endpoint to https

--- src/services/oraclePoolState.service.ts
+++ src/services/oraclePoolState.service.ts
@@ -4,7 +4,7 @@ import { GET_ORACLE_POOL_DATA_STRUCT } from '../constants/struct.types';
 export const pools = {
   ergusd: 'https://erg-oracle-ergusd.spirepools.com/frontendData',
   adausd: 'https://ada-usd-ergo-oracle.emurgo.io/frontendData',
-  ethusd: 'http://www.ergolui.com/eth-usd-oracle/frontendData',
+  ethusd: 'https://ergolui.com/eth-usd-oracle/frontendData',
 };
 export class OraclePoolStateService {
   static getPoolData(dispatch: any, poolType: any): any {
