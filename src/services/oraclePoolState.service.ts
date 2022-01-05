59a6a9f400dfa94a754b26df30e779927f1ab730
deadit <stdeadit@gmail.com>
2022-01-05 16:09:12 +0300

    new version

--- src/services/oraclePoolState.service.ts
+++ src/services/oraclePoolState.service.ts
@@ -2,7 +2,7 @@ import { fetchStruct } from '../utils/fetchStruct';
 import { GET_ORACLE_POOL_DATA_STRUCT } from '../constants/struct.types';
 
 export const pools = {
-  ergusd: 'https://oracle-core.ergopool.io/frontendData',
+  ergusd: 'https://erg-oracle-ergusd.spirepools.com/frontendData',
   adausd: 'https://ada-usd-ergo-oracle.emurgo.io/frontendData',
   ethusd: 'https://ergolui.com/eth-usd-oracle/frontendData',
 };
