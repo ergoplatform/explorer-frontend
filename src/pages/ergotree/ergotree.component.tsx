7a7c884996b5bd0e08c9a8ae1794655ec97f5182
Jozef Koval <jozef.koval@protonmail.ch>
2022-09-30 11:48:55 +0200

    Create ergotree view for conversion into human readable form.

---
+++ src/pages/ergotree/ergotree.component.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+import environment from 'src/config/environment';
+import './ergotree.scss';
+
+export default class ErgotreeComponent extends React.Component<{}, {hashedScript: string, constants: string, humanScript: string}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      hashedScript: 'Please paste your favorite hashed ergo script.',
+      constants: '',
+      humanScript: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event: { target: { value: string; }; }) {
+    this.setState({hashedScript: event.target.value, humanScript: this.state.humanScript});
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+    axios
+      .post(`${environment.apiUrlV1}/ergotree/convert`, { hashed: this.state.hashedScript })
+      .then((response: AxiosResponse) => {
+        if (!response) {
+          return Promise.reject(
+            `Address api service. Request: ${environment.apiUrlV1}/ergotree/convert`
+          );
+        }
+        this.setState({hashedScript: this.state.hashedScript, constants: response.data.constants, humanScript: response.data.script});
+        return Promise.resolve("");
+      });
+  }
+
+  render() {
+    return (
+      <div style={{padding: "15px"}}>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <textarea className="bi-textarea" value={this.state.hashedScript} onChange={this.handleChange} />
+          </div>
+          <div>
+            <input type="submit" value="Convert to human readable script" />
+          </div>
+        </form>
+        <h1>Constants</h1>
+        <pre>{this.state.constants}</pre>
+        <h1>Human readable script</h1>
+        <pre>{this.state.humanScript}</pre>
+      </div>
+    );
+  }
+}
